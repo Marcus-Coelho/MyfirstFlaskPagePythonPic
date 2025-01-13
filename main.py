79659192c5d4d2196b4eecd81cf0a81f7832caa0
Coelho007 <marcuscoelho07@gmail.com>
2025-01-13 17:42:10 -0300

    Corrigindo app run debug to false

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     
     # Configurar o host para "0.0.0.0" para que o Railway consiga acessar o servidor
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
 
