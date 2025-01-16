0dcffbf7963fac8be0817e94fac2bb3810d04f0c
Coelho007 <marcuscoelho07@gmail.com>
2025-01-16 13:25:33 -0300

    comunidade.db roo

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(app.instance_path, "comunidade.db")}'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
